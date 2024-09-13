0811c06f2257b3fa0be34b6104544ebbd892ee84
Dherisya <dherisyanovianakurniawan@gmail.com>
2024-09-13 07:47:17 +0700

    menambah file

--- database/migrations/2024_09_11_015131_create_hewans_table.php
+++ database/migrations/2024_09_11_015131_create_hewans_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use function Laravel\Prompts\table;
-
 return new class extends Migration
 {
     /**
@@ -15,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('hewans', function (Blueprint $table) {
             $table->id();
-            $table->string("jenis_hewan",20);
-            $table->string("jenis_makanan",30);
+            $table->string("jenis_hewan", 20);
+            $table->string("jenis_makanan", 30);
             $table->integer("umur_hewan");
             $table->string("deskripsi")->nullable();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RumahController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TableController;
 use App\Http\Controllers\StoreController;
+use App\Models\product;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
@@ -23,6 +24,8 @@ Route::get('/form', function () {
 //Route::get('/home', [HomeController::class, 'index']); // Untuk menampilkan form
 //st('/home', [HomeController::class, 'submit']); // Untuk mengirim form
 
-//Route::get("/create", [StoreController::class, "store"]);
+Route::get("/create", [StoreController::class, "store"]);
 
 Route::get("/show", [TableController::class, "show"]);
+
+Route::get('/product/{id}/edit', [TableController::class, 'edit']);
