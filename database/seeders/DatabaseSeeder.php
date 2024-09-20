969ea15ad9ab53b5a164e1e608f2235d1f066a30
Dherisya <dherisyanovianakurniawan@gmail.com>
2024-09-20 15:52:29 +0700

    menambah file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call(HumanSeeder::class);
-        
+
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
