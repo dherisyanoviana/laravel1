7fddde65ee117eb450797b3ab322c3bf839f75f6
Dherisya <dherisyanovianakurniawan@gmail.com>
2024-09-20 07:43:20 +0700

    menambah file

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 class SchoolController extends Controller
 {
     public function index() {
-
         $data = teacher::latest()->get();
         return view("schoolTable", compact("data"));
     }
--- resources/views/schoolTable.blade.php
+++ resources/views/schoolTable.blade.php
@@ -4,37 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Daftar Guru dan Siswa</title>
+    {{-- <title>Daftar Guru dan Siswa</title> --}}
+    <title>Daftar Siswa dan Extrakurikuler</title>
 </head>
 <body>
-    <h1>Daftar Guru dan Siswa</h1>
+    {{-- <h1>Daftar Guru dan Siswa</h1> --}}
+    <h1>Daftar Siswa dan Extrakurikuler</h1>
 
     <table border="1" cellpading="5" cellspacing="0">
         <thead>
             <tr>
-                <th>Nama Guru</th>
+                {{-- <th>Nama Guru</th>
                 <th>Mata Pelajaran</th>
-                <th>Daftar Siswa</th>
+                <th>Daftar Siswa</th> --}}
+                <th>Nama Siswa</th>
+                <th>Ekstrakurikuler yang diikuti</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($data as $teacher)
+            {{-- @foreach ($data as $teacher) --}}
+            @foreach ($data as $siswa )
+
+            @endforeach
             <tr>
-                <td>{{ $teacher->nama }}</td>
-                <td>{{ $teacher->mapel }}</td>
+                {{-- <td>{{ $teacher->nama }}</td>
+                <td>{{ $teacher->mapel }}</td> --}}
+                <td>{{ $siswa->nama }}</td>
                 <td>
                     <ul>
-                        @if ($teacher->student->isNotEmty())
+                        {{-- @if ($teacher->student->isNotEmty())
                             @foreach ($teacher->student as $student)
-                            <li>{{ $student->nama }}</li>
+                            <li>{{ $student->nama }}</li> --}}
+                            @if ($siswa->extra->isNotEmty())
+                            @foreach ($siswa->$extra as $extra )
+                            <li>{{ $extra->nama }} - {{ $extra->jadwal }}</li>
                             @endforeach
                             @else
-                            <li><em>Tidak ada Siswa</em></li>
+                            {{-- <li><em>Tidak ada Siswa</em></li> --}}
+                            <li>Tidak mengikuti extrakurikuler</li>
                         @endif
                     </ul>
                 </td>
             </tr>
-            @endforeach
+
         </tbody>
     </table>
 </body>
